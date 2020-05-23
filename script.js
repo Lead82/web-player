f335f690c3ddf364b7422f9714bc6028f9759000
Andrea Pisu <pisuandrea99@gmail.com>
2020-05-23 12:32:18 +0200

    Player controls management

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const lyricsText = "Turn your magic on, Umi she'd say\r\nEverything you want's a dream away\r\nWe are legends, every day\r\n\nThat's what she told me\r\nTurn your magic on, to me she'd say\n\nEverything you want's a dream away\n\nUnder this pressure, under this weight\n\nWe are diamonds \n\n\n\nI feel my heart beating\n\nI feel my heart underneath my skin\n\nI feel my heart beating\n\nOh, you make me feel\n\nLike I'm alive again\n\n\n\nAlive again!\n\n\n\nOh, you make me feel\n\nLike I'm alive again\n\n\n\nSaid I can't go on, not in this way\n\nI'm a dream that died by light of day\n\nGonna hold up half the sky and say\n\nOnly I own me\n\n\n\nI feel my heart beating\n\nI feel my heart underneath my skin\n\nOh, I can feel my heart beating\n\n'Cause you make me feel\n\nLike I'm alive again\n\n\n\nAlive again!\n\n\n\nOh, you make me feel\n\nLike I'm alive again\n\n\n\nTurn your magic on, Umi she'd say\n\nEverything you want's a dream away\n\nUnder this pressure, under this weight\n\nWe are diamonds taking shape\n\nWe are diamonds taking shape\n\n\n\n(Woo, woo)\n\n\n\nIf we've only got this life\n\nThis adventure, oh then I\n\nAnd if we've only got this life\n\nYou'll get me through alive\n\nAnd if we've only got this life\n\nIn this adventure, oh then I\n\nWanna share it with you\n\nWith you, with you\n\nI said, oh, say oh\n\n\n\n(Woo hoo, woo hoo...)"
 
 const textContainer = document.querySelector(".lyrics-text")
+const playButton = document.querySelector(".play-pause")
 
-const text = document.createElement('p')
-text.innerHTML = lyricsText.replace(/(?:\r\n|\n\n\n\n|\r|\n)/g, '<br>')
+const text = document.createElement("p")
+var convertedNewLines = lyricsText.replace(/(?:\r\n|\r|\n)/g, '<br>')
+text.innerHTML = convertedNewLines.replace(/(?:<br><br><br>|<br><br><br><br>)/g, '<br><br>. . .<br>')
 textContainer.appendChild(text)
+
+playButton.addEventListener('click', function(){
+    var imgButton = playButton.children[0]
+    if(imgButton.getAttribute("alt") == "pause"){
+        imgButton.src = "assets/play.svg"
+        imgButton.alt = "play"
+    }else{
+        imgButton.src = "assets/pause.svg"
+        imgButton.alt = "pause"
+    }
+})
